cecf2be4cec08d3a30deb296f9d1beacee753054
Ajay <102792088+ajay-dv8@users.noreply.github.com>
2024-11-10 14:48:57 +0000

    Update cards-carousel.tsx border color change

--- components/ui/cards-carousel.tsx
+++ components/ui/cards-carousel.tsx
@@ -241,7 +241,7 @@ export const Card = ({
       <motion.button
         layoutId={layout ? `card-${card.title}` : undefined}
         onClick={handleOpen}
-        className="rounded-xl bg-gray-100 dark:bg-neutral-900 h-80 w-56 md:h-[40rem] md:w-96 border border-white overflow-hidden flex flex-col items-start justify-start relative z-10"
+        className="rounded-xl bg-gray-100 dark:bg-neutral-900 h-80 w-56 md:h-[40rem] md:w-96 border border-[#1f2fe6] overflow-hidden flex flex-col items-start justify-start relative z-10"
       >
         <div className="absolute h-full top-0 inset-x-0 bg-gradient-to-b from-black/50 via-transparent to-transparent z-30 pointer-events-none" />
 
