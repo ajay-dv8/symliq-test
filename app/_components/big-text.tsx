e5919e8e5f3d71e036806f981d5eff880568be56
Ajay <102792088+ajay-dv8@users.noreply.github.com>
2024-11-10 12:04:28 +0000

    Update big-text.tsx

--- app/_components/big-text.tsx
+++ app/_components/big-text.tsx
@@ -2,7 +2,7 @@ export const BigText = () => {
   return(
     <div className="w-full flex justify-center bg-blue ">
       <div className="w-[90%] md:w-[80%] flex justify-center py-12 px-20 md:px-40 bg-[#1f2fe6] rounded-xl"> 
-        <h1 className="text-3xl md:text-6xl text-white text-center font-extrabold uppercase"> 
+        <h1 className="text-3xl md:text-6xl text-white text-center font-sans uppercase"> 
           We craft experiences that capture attention and build lasting engagement with your brand.
         </h1>
       </div>
